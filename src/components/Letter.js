4bc88e61507e8cb2193cfb13a21cffb6249b05e8
Adam Wajahat <adamwonline0@yahoo.com>
2023-10-14 12:28:30 -0500

    Final website

---
+++ src/components/Letter.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "../styles/Letter.css"
+
+function Letter() {
+  return (
+    <>
+      <div className="letter">
+          <div className='bismillah'>Bismillahir Rahmanir Raheem</div>
+          <br></br>
+          <br></br>
+          <div className='italicFont'>Dear Humanity,</div>
+          <br></br>
+          <div>Welcome to the world. The world not seen around you but the world that is shown to you. The world where the oppressed stands as the villian and the oppressor is paraded as the hero. The world where a cry for help from the weak lands on deaf ears and the scenes of massacres are blinded by the tv screens. The world where no one knows what psyops is but everyone believes in it.</div>
+          <br></br>
+          <div>Welcome to where falsehood has become truth and truth has become falsehood.</div>
+          <br></br>
+          <div><span className='italicFont'> Humanity?</span> I am dying. Can you feel the pain or not? <span className='italicFont'>Where have you gone? Why have you forgotten me?</span> As the war planes fly over our homes and food sources run dry, you stand around. Years of this cycle but now, this oppression, has murdered my name, the weak, as the tryant.</div>
+          <br></br>
+          <div>To find me please, you first have to find the truth. Educate yourself and do not consume what you are shown. Then work on those around you. One individual at a time.</div>
+          <br></br>
+          <div>Palestine, I am sorry. </div>
+          <br></br>
+          <br></br>
+          <div className='ending'><span className='italicFont'>Allah please accept our duaas and forgive us for our weakness,</span>
+          <br></br>
+          Oct 12, 2023</div>
+          <br></br>
+          <br></br>
+      </div>
+      <div className='sourceList'>
+        <div className='bismillah'>An idea of where to start</div>
+        <a href='https://www.aljazeera.com/news/2023/10/9/whats-the-israel-palestine-conflict-about-a-simple-guide'>A quick read on the history of the Palestinian and Israeli conflict</a>
+        <br></br>
+        <a href='https://israelpalestinenews.org/palestinian-resistance-launches-surprise-attack-on-israel-updates-to-come/'>Perspective on how one-sided this looks</a>
+        <br></br>
+        <a href='https://www.youtube.com/watch?v=c6c5W2RsvkE'>A deeper look at the roots of the land of Palestine back from the Crusades</a>
+        <br></br>
+        <a href='https://www.youtube.com/watch?v=8O2VRmwIzXA&pp=ygULZXBpYyBtYXNqaWQ%3D'>Contextualizing the Palestinian conflict in today's time</a>
+        <br></br>
+        <a href='https://en.wikipedia.org/wiki/Psychological_operations_(United_States)'>How the media is playing its part right now</a>
+      </div>
+    </>
+  )
+}
+
+export default Letter
---
+++ src/components/PhoneNumber.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, push } from 'firebase/database';
+import "../styles/PhoneNumber.css"
+import FirebaseContext from '../pages/Home'
+
+
+function PhoneNumber() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [submitted, setSubmitted] = useState(false)
+  const database = useContext(FirebaseContext);
+  
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const timestamp = Date.now();
+    const date = new Date(timestamp);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    const formattedDate = date.toLocaleString('en-US', options);
+
+    console.log(database)
+
+    push(ref(database, '/phoneNumber'), {[formattedDate]: phoneNumber});
+
+    // Clear the input field
+    setPhoneNumber('');
+
+    setSubmitted(true);
+  }
+
+  function handleChange(event) {
+    setPhoneNumber(formatPhoneNumber(event.target.value));
+  }
+
+  function formatPhoneNumber(value) {
+    const phoneNumber = value.replace(/\D/g, '');
+    const phoneNumberLength = phoneNumber.length;
+  
+    if (phoneNumberLength < 4) {
+      return phoneNumber;
+    } else if (phoneNumberLength < 7) {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    } else {
+      return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`;
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Updates
+      </label>
+      <div className='phoneNumberInput'>
+        <input placeholder='(123) 456-7890'type="tel" value={phoneNumber} onChange={handleChange} />
+        <button disabled={phoneNumber.replace(/\D/g, '').length === 10 && !submitted ? false : true} type="submit">{submitted ? 'Submitted' : 'Submit'}</button>
+      </div>
+    </form>
+  );
+}
+
+export default PhoneNumber
